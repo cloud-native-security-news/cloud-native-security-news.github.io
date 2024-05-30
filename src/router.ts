ff656a48846f6628805f78ba105e987ae8630f04
LEI WANG <ssst0n3@gmail.com>
2024-05-30 17:44:29 +0800

    try hello
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import {createRouter, createWebHistory, Router, RouteRecordRaw} from 'vue-router'
 import Github from "@/components/Github.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 const routes: RouteRecordRaw[] = [
     {path: '/github', component: Github},
+    {path: '/hello', component: HelloWorld},
 ]
 
 const router: Router = createRouter({
